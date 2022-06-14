6d2b412980d33d2a56d455bdaf3fd01046d245ce
Lilian Burdianov <lilian.burdianov@gmail.com>
2022-06-14 15:11:54 +0400

    complete app

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  '0x42E356326a2F6cfd001BbE81ff6e9D654438FD6C';
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
